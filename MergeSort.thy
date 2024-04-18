76391cb41e0df8f4fbee63fd4c73e7d008fe2b22
Mike Sperber <sperber@deinprogramm.de>
2024-04-18 09:33:14 +0200

    Update.

--- MergeSort.thy
+++ MergeSort.thy
@@ -12,4 +12,24 @@ fun merge :: "('a :: linorder) list \<Rightarrow> 'a list \<Rightarrow> 'a list"
        (if x \<le> y 
         then x # (merge xs (y # ys))
         else y # (merge (x # xs) ys))"
+
+fun mergesort :: "('a :: linorder) list \<Rightarrow> 'a list"
+  where
+    "mergesort [] = []"
+  | "mergesort [x] = [x]"
+  | "mergesort xs =
+     (let half = (length xs) div 2
+      in merge (mergesort (take half xs)) (mergesort (drop half xs)))"
+
+(* Ist ein Element \<le> alle Elemente einer Liste? *)
+fun less_equal_list :: "'a \<Rightarrow> ('a :: linorder) list \<Rightarrow> bool"
+  where
+    "less_equal_list x [] = True"
+  | "less_equal_list x (y # ys) =
+      (x \<le> y \<and> less_equal_list x ys)"
+(*
+     (if x \<le> y
+      then less_equal_list x ys
+      else False)"
+*)
 end
