145fa2dc22a445c60149f3561fbde3df7f8880f0
Mike Sperber <sperber@deinprogramm.de>
2024-04-18 09:24:13 +0200

    Update.

--- MergeSort.thy
+++ MergeSort.thy
@@ -1,4 +1,15 @@
 theory MergeSort
   imports Main
 begin
+
+(* linorder: 'a ist eine totale Ordnung, mit \<le> *)
+
+fun merge :: "('a :: linorder) list \<Rightarrow> 'a list \<Rightarrow> 'a list"
+  where
+      "merge [] ys = ys"
+    | "merge xs [] = xs"
+    | "merge (x # xs) (y # ys) =
+       (if x \<le> y 
+        then x # (merge xs (y # ys))
+        else y # (merge (x # xs) ys))"
 end
